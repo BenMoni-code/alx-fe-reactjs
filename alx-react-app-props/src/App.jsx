4a2cdc6f644dc6af7d064286834cfa4dee7dd8fd
Omar Mohumed <wadaniinarais1@gmail.com>
2025-09-07 21:43:15 +0300

    added alx-react-app-props

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,25 +1,14 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-import './App.css'
-import UserContext from './components/UserContext';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
-  const userData = { name:"Alice", age:"25", bio:"Loves hiking and photography" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserContext.Provider value={userData}>
-        <UserProfile />
-      </UserContext.Provider>
-    </>
-  )
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
